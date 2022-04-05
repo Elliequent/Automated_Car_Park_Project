02d3d855daa89c338c2ea3e0b3998ec8103f7791
Ian Fraser <iFraser24@Gmail.com>
2022-04-05 19:05:03 +0100

    Database restart function

---
+++ 1 - Website Development/System/Functions/delete_database.php
@@ -0,0 +1,35 @@
+<?php
+
+function deletedatabase()
+{
+
+    $servername = "178.79.190.121:3306";
+    $username = "admin";
+    $password = "OMEGA44DAYS00#";
+
+    // IP Address, username, password and database table
+    $con = mysqli_connect($servername, $username, $password); 
+
+    $sql = "DROP DATABASE IP3_PP";
+
+    if (!mysqli_query($con, $sql)) 
+    {
+
+        // TEMP - Redirect to system down page
+        echo "Error deleting database: " . mysqli_error($con);
+
+    }   // End of IF (mysqli_query($con, $sql))
+    else
+    {
+
+        echo "Database deleted - redirecting and repopulating database";
+        header("refresh: 5; url = ../../index.php");
+        exit();
+
+    }
+
+}   // End of FUNCTION seedDatabase_part1()
+
+deletedatabase();
+
+?>
